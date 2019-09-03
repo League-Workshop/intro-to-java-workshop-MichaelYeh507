32af6d334f2b59f413080c5841983702b7c682b6
League Student <league@iMac-29.attlocal.net>
2019-09-02 18:28:37 -0700

    Today i finished the whole section 3, and worked on a project in section 4.

--- src/section3/SpeakAndSpell.java
+++ src/section3/SpeakAndSpell.java
@@ -9,15 +9,31 @@ public class SpeakAndSpell {
 
 	public static void main(String[] args) {
 		// 1. Use the speak method to say the word. "e.g. spell mandlebrot"
-
+speak("e.g. spell mandlebrot");
 		// 2. Catch the user's answer in a String
+String answer =  JOptionPane.showInputDialog("How do you spell this?");
 
 		// 3. If the user spelled the word correctly, speak "correct"
-
+if (answer.equals ("mandlebrot")) {
+speak("Correct!");
+}
 		// 4. Otherwise say "wrong"
-
+else {
+speak("Wrong");
+}
 		// 5. repeat the process for other words
-		
+speak("e.g. spell elephant");
+// 2. Catch the user's answer in a String
+answer =  JOptionPane.showInputDialog("How do you spell this?");
+
+// 3. If the user spelled the word correctly, speak "correct"
+if (answer.equals ("elephant")) {
+speak("Correct!");
+}
+// 4. Otherwise say "wrong"
+else {
+speak("Wrong");
+}
 	}
 
 	static void speak(String words) {
